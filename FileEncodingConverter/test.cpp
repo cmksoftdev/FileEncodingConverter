a5ba924bcf68eb2431b1d016576e3fe01e50e482
cmk1988 <cmk88@web.de>
2018-05-17 00:03:58 +0200

    code cleaned up

--- FileEncodingConverter/test.cpp
+++ FileEncodingConverter/test.cpp
@@ -5,8 +5,7 @@ vector<char> get_file_content(string filename)
 {
 	ifstream ifs(filename, ios::binary | ios::ate);
 	ifstream::pos_type pos = ifs.tellg();
-
-	vector<char>  result(pos);
+	vector<char> result(pos);
 
 	ifs.seekg(0, ios::beg);
 	ifs.read(&result[0], pos);
@@ -17,7 +16,8 @@ vector<char> get_file_content(string filename)
 
 int test_file()
 {
-	char expected[] = { 
+	char expected[] = 
+	{ 
 		0xC3, 0xB6, 0xC3, 0xBC, 0xC3, 0xA4, 0x20, 0x48,
 		0x61, 0x6C, 0x6C, 0x6F, 0x20, 0x68, 0x61, 0x6C,
 		0x6C, 0x6F, 0x20, 0xC3, 0xB6, 0xC3, 0xBC, 0xC3,
@@ -27,16 +27,15 @@ int test_file()
 
 	if (file.size() != 32) return -2;
 	for (size_t i = 0; i < file.size(); i++)
-	{
 		if (file.at(i) != expected[i]) return -1;
-	}
 	return 0;
 }
 
 void create_file()
 {
 	ofstream ofs("input.txt");
-	char filecontent[] = {
+	char filecontent[] = 
+	{
 		0xF6, 0xFC, 0xE4, 0x20, 0x48, 0x61, 0x6C, 0x6C,
 		0x6F, 0x20, 0x68, 0x61, 0x6C, 0x6C, 0x6F, 0x20,
 		0xF6, 0xFC, 0xE4, 0x20, 0xE4, 0xFC, 0xF6, 0x00
@@ -49,10 +48,10 @@ int main()
 {
 	//Arrange
 	create_file();
+	FileEncodingConverter sut;
 
 	//Act
-	auto sut = new FileEncodingConverter();
-	sut->convert_win1252_to_utf8("input.txt", "output.txt");
+	sut.convert_win1252_to_utf8("input.txt", "output.txt");
 
 	//Assert to be 0
 	return test_file();
