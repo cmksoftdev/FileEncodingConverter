af017e28f2cb986073f0e29a4f661812f13d5e93
cmk1988 <cmk88@web.de>
2018-05-16 23:37:07 +0200

    test added

--- FileEncodingConverter/test.cpp
+++ FileEncodingConverter/test.cpp
@@ -1,7 +1,59 @@
 #include "FileEncodingConverter.h"
+#include <vector>
 
-void main()
+vector<char> get_file_content(string filename)
 {
-	auto converter = new FileEncodingConverter();
-	converter->convert_win1252_to_utf8("test.txt", "output.txt");
+	ifstream ifs(filename, ios::binary | ios::ate);
+	ifstream::pos_type pos = ifs.tellg();
+
+	vector<char>  result(pos);
+
+	ifs.seekg(0, ios::beg);
+	ifs.read(&result[0], pos);
+	ifs.close();
+
+	return result;
+}
+
+int test_file()
+{
+	char expected[] = { 
+		0xC3, 0xB6, 0xC3, 0xBC, 0xC3, 0xA4, 0x20, 0x48,
+		0x61, 0x6C, 0x6C, 0x6F, 0x20, 0x68, 0x61, 0x6C,
+		0x6C, 0x6F, 0x20, 0xC3, 0xB6, 0xC3, 0xBC, 0xC3,
+		0xA4, 0x20, 0xC3, 0xA4, 0xC3, 0xBC, 0xC3, 0xB6 
+	};
+	auto file = get_file_content("output.txt");
+
+	if (file.size() != 32) return -2;
+	for (size_t i = 0; i < file.size(); i++)
+	{
+		if (file.at(i) != expected[i]) return -1;
+	}
+	return 0;
+}
+
+void create_file()
+{
+	ofstream ofs("input.txt");
+	char filecontent[] = {
+		0xF6, 0xFC, 0xE4, 0x20, 0x48, 0x61, 0x6C, 0x6C,
+		0x6F, 0x20, 0x68, 0x61, 0x6C, 0x6C, 0x6F, 0x20,
+		0xF6, 0xFC, 0xE4, 0x20, 0xE4, 0xFC, 0xF6, 0x00
+	};
+	ofs << filecontent;
+	ofs.close();
+}
+
+int main()
+{
+	//Arrange
+	create_file();
+
+	//Act
+	auto sut = new FileEncodingConverter();
+	sut->convert_win1252_to_utf8("input.txt", "output.txt");
+
+	//Assert to be 0
+	return test_file();
 }
